e120c444f4c7af5028bd0990f615026c04875645
Raphael Michel <mail@raphaelmichel.de>
2021-04-24 19:38:17 +0200

    Initial commit

---
+++ pretix_cwa/__init__.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+__version__ = "1.0.0"
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_cwa"
+    verbose_name = "CWA integration"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("CWA integration")
+        author = "pretix team"
+        description = gettext_lazy("pretix integration for the Corona Warn App (CWA)")
+        visible = True
+        version = __version__
+        category = "INTEGRATION"
+        compatibility = "pretix>=3.17.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = "pretix_cwa.PluginApp"
