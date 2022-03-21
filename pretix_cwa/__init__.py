86bc7995e91e5159c8e73540be5e2248d737fa57
Raphael Michel <michel@rami.io>
2022-03-21 17:00:19 +0100

    Udpate plugin metadata

--- pretix_cwa/__init__.py
+++ pretix_cwa/__init__.py
@@ -15,7 +15,8 @@ class PluginApp(PluginConfig):
     class PretixPluginMeta:
         name = gettext_lazy("CWA integration")
         author = "pretix team"
-        description = gettext_lazy("pretix integration for the Corona Warn App (CWA)")
+        description = gettext_lazy("Integrating pretix with the Corona Warn App (CWA) allows you to generate CWA event QR codes for your event on demand and email them to attendees after check-in.")
+        picture = "pretix_cwa/logo.svg"
         visible = True
         version = __version__
         category = "INTEGRATION"
